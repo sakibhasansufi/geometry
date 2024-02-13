40f4e9ee8b81ffe77097c01676e76956a025ecbd
sakibhasansufi <sakibhasansufi@gmail.com>
2024-02-14 00:33:16 +0600

    now

--- javascript/calculation.js
+++ javascript/calculation.js
@@ -9,7 +9,7 @@ function calculateTriangleArea() {
     const triangleHeightValue = triangleHeightInput.value;
     const height = parseFloat(triangleHeightValue);
     console.log(height);
-    
+
     const area = 0.5 * base * height;
     console.log('the area is', area);
 
@@ -35,21 +35,47 @@ function calculateRectangleArea() {
     rectangleArea.innerText = area;
 };
 
-
 // parallelogram calculation
+
 function parallelogramArea() {
-    const parallelogrmBaseInput = document.getElementById('parallelogram-base');
-    const parallelogrmInput = parallelogrmBaseInput.value;
-    const base = parseFloat(parallelogrmInput);
-    console.log(base)
-    const parallelogramHeightInput = document.getElementById('parallelogram-height');
-    const parallelogramInput = parallelogramHeightInput.value;
-    const height = parseFloat(parallelogramInput);
-    console.log(height);
+    const parallelogrmBaseInput = getInputValue('parallelogram-base');
+    const parallelogramHeightInput = getInputValue('parallelogram-height');
+    const area = parallelogrmBaseInput * parallelogramHeightInput;
+    setInnerTextById('area-result', area)
+}
 
-    const area = base * height;
-    console.log(area);
+function getInputValue(inputFieldId) {
+    const inputId = document.getElementById(inputFieldId);
+    const inputText = inputId.value;
+    const inputValue = parseFloat(inputText);
+    return inputValue;
 
-    const areaResult = document.getElementById('area-result');
-    areaResult.innerText = area;
+};
+
+
+function setInnerTextById(elementId, text) {
+    const element = document.getElementById(elementId);
+    element.innerText = text;
+}
+
+
+// rhoumbus area calculation 
+function rhombusAreaButton() {
+    const d1 = getRhoumbusValue('rhombus-d1');
+    const d2 = getRhoumbusValue('rhombus-d2');
+    const res = 0.5 * d1 * d2;
+    setInnerTextById('rhoumbus-result',res)
+
+}
+
+function getRhoumbusValue(input) {
+    const rhoumbusD1 = document.getElementById(input);
+    const d1Text = rhoumbusD1.value;
+    const d1Value = parseFloat(d1Text);
+    return d1Value;
+}
+
+function rhoumbusResult (resultRhoumbus, put){
+    const res = document.getElementById(resultRhoumbus);
+    res.innerText = put;
 }
